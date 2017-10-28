00ea4a9d31931e364091eae77a57c52df272b499
Michael Iacona <Mike-Iacona@users.noreply.github.com>
2017-10-28 14:44:32 -0400

    unit tests

--- unittest-mi.py
+++ unittest-mi.py
@@ -13,15 +13,15 @@ class miTest(unittest.TestCase):
 
     def test_us09(self):
         self.assertEqual(mi.us09("testfiles/test-us09.ged"),
-        ['Error US09: Death date of Alex /Jones/ (3 JAN 1938) occurs before birth date'])
+        	['Error US09: Death date of Alex /Jones/ (3 JAN 1938) occurs before birth date'])
 
     def test_us12(self):
         self.assertEqual(mi.us12("testfiles/test-us12.ged"),
-        ['Error US12: Father Alex /Jones/ (I01) is at least 80 years older than his child'])
+        	['Error US12: Father Alex /Jones/ (I01) is at least 80 years older than his child'])
 
     def test_us21(self):
         self.assertEqual(mi.us21("testfiles/test-us12.ged"),
-        ['Error US21: Gender of Alex /Jones/ (I01) does not match family role', 'Error US21: Gender of Lacy /jones/ (I07) does not match family role'])       
+        	['Error US21: Gender of Alex /Jones/ (I01) does not match family role', 'Error US21: Gender of Lacy /jones/ (I07) does not match family role'])       
 
 
 
