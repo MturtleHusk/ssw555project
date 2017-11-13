d574b2dfc79e21d361afefbb172cb3bb905827a6
Michael Iacona <Mike-Iacona@users.noreply.github.com>
2017-11-12 20:50:25 -0500

    unit test fixed

--- unittest-mi.py
+++ unittest-mi.py
@@ -34,16 +34,16 @@ class miTest(unittest.TestCase):
 
 
 
-     def test_us34(self):
+    def test_us29(self):
         with open ("testfiles/test-us29.ged") as f:
             parsed = parse_ged(f.read().split('\n'))
-            self.assertEqual(mi.us21(parsed),
+            self.assertEqual(mi.us29_list(parsed),
                              ['US29: List of deceased individuals:', ['Alex /Jones/ (I01)', 'Lacy /jones/ (I07)']])
 
-     def test_us34(self):
+    def test_us34(self):
         with open ("testfiles/test-us34.ged") as f:
             parsed = parse_ged(f.read().split('\n'))
-            self.assertEqual(mi.us21(parsed),
+            self.assertEqual(mi.us34_list(parsed),
                              ['US34: List couples married with one spouse twice the age of the other:', ['Alex /Jones/ and Lacy /jones/ (F23)']])
 if __name__ == '__main__':
     unittest.main()
